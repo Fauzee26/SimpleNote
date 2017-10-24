415077551ec4b6e60c2cdbc0b9cd25f3f2eec5c6
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2017-10-24 14:34:59 +0700

    Add New Task with Core Data

---
+++ Simple Note/Simple Note/AddTaskViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AddTaskViewController.swift
+//  Simple Note
+//
+//  Created by Muhammad Hilmy Fauzi on 24/10/17.
+//  Copyright © 2017 Hilmy Corp. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    @IBOutlet weak var etNameTask: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSaveTask(_ sender: Any) {
+        //declare context
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //declare task
+        let nameTask = Task(context: context) //declare nameTask as context from entyty task
+        nameTask.name_task = etNameTask.text //declare is the nameTask.name_task field is from etNameTask.text
+        
+         // Save the data to coredata
+        //check are value from etTask is nil or not
+        if etNameTask.text == "" {
+            //condition if nil
+            //display alert dialog
+            let alertController = UIAlertController(title: "Warning",
+                                                    message: "Input Profile Can't Be Empty", preferredStyle : .alert)
+            alertController.addAction(UIAlertAction(title: "OK",
+                                                    style: .default,handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }else{
+            //condition if task text is filled
+            //data saved to core data
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            _ = navigationController?.popViewController(animated: true)
+            
+            //print if the data has been added
+            print("Data has been added")
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
